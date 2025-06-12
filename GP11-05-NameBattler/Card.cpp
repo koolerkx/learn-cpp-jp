859221731c90fd5744a75586cd6ce986242ad363
Kooler Fan <koolerkx@gmail.com>
2025-06-12 15:25:47 +0900

    refine the card multiplier logic

--- GP11-05-NameBattler/Card.cpp
+++ GP11-05-NameBattler/Card.cpp
@@ -19,11 +19,7 @@ float AttackCard::calculate_score(const PlayerHeroAI& ph) const
 
 int AttackCard::apply_card(PlayerHero& user, PlayerHero& target, float multiply) const
 {
-    // based on multiply(0-2), attack bias from 100% to 200%
-    float bias = 1.0f + multiply * 0.5f;
-    int offset = static_cast<int>(static_cast<float>(user.get_attack()) * bias);
-    
-    int power = power_ + offset;
+    int power = static_cast<int>(static_cast<float>(power_) * multiply);
     target.take_damage(power);
 
     return power;
@@ -45,11 +41,7 @@ float HealCard::calculate_score(const PlayerHeroAI& ph) const
 
 int HealCard::apply_card(PlayerHero& user, PlayerHero&, float multiply) const
 {
-    // based on multiply(0-2), healing offset from HP 15% to 45% 
-    float bias = 0.15f + multiply * 0.15f;
-    int offset = static_cast<int>(static_cast<float>(user.get_max_hp()) * bias);
-    
-    int power = power_ + offset;
+    int power = static_cast<int>(static_cast<float>(power_) * multiply);
     user.take_heal(power);
 
     return power;
@@ -70,10 +62,7 @@ float DefenseCard::calculate_score(const PlayerHeroAI& ph) const
 
 int DefenseCard::apply_card(PlayerHero& user, PlayerHero&, float multiply) const
 {
-    float bias = 0.1f + multiply * 0.2f;
-    int offset = static_cast<int>(static_cast<float>(user.get_defense()) * bias);
-
-    int power = power_ + offset;
+    int power = static_cast<int>(static_cast<float>(power_) * multiply);
     user.take_shield(power);
 
     return power;
